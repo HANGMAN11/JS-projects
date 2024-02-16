ac47568ced8e5f3d93f91d872c64ae7103e73c82
Oleh Rytov <oleja.229@gmail.com>
2024-02-16 23:52:24 +0200

    added arrays to determine collision calculations in the future

--- Virtual city/car/car.js
+++ Virtual city/car/car.js
@@ -7,55 +7,56 @@ class Car {
 
     this.speed = 0;
     this.acceleration = 0.2;
-    this.maxSpeed = 3;
+    this.maxSpeed = 7;
     this.friction = 0.05;
     this.angle = 0;
 
+    this.sensor = new Sensor(this);
     this.controls = new Controls();
   }
 
-  update() {
+  update(roadBorders) {
     this.#move();
+    this.sensor.update(roadBorders);
   }
-  
-  #move(){
+
+  #move() {
     if (this.controls.forward) {
-        this.speed += this.acceleration;
-      }
-      if (this.controls.reverse) {
-        this.speed -= this.acceleration;
-      }
-      
-  
-      if (this.speed > this.maxSpeed) {
-        this.speed = this.maxSpeed;
-      }
-  
-      if (this.speed < -this.maxSpeed / 2) {
-        this.speed = -this.maxSpeed / 2;
-      }
-  
-      if (this.speed > 0) {
-        this.speed -= this.friction;
-      }
-      if (this.speed < 0) {
-        this.speed += this.friction;
-      }
-      if (Math.abs(this.speed) < this.friction) {
-        this.speed = 0;
-      }
-      if(this.speed!=0){
-          const flip = this.speed>0?1:-1;
-      
+      this.speed += this.acceleration;
+    }
+    if (this.controls.reverse) {
+      this.speed -= this.acceleration;
+    }
+
+    if (this.speed > this.maxSpeed) {
+      this.speed = this.maxSpeed;
+    }
+
+    if (this.speed < -this.maxSpeed / 2) {
+      this.speed = -this.maxSpeed / 2;
+    }
+
+    if (this.speed > 0) {
+      this.speed -= this.friction;
+    }
+    if (this.speed < 0) {
+      this.speed += this.friction;
+    }
+    if (Math.abs(this.speed) < this.friction) {
+      this.speed = 0;
+    }
+    if (this.speed != 0) {
+      const flip = this.speed > 0 ? 1 : -1;
+
       if (this.controls.left) {
-          this.angle += 0.03*flip;
-        }
-        if (this.controls.right) {
-          this.angle -= 0.03*flip;
-        }
+        this.angle += 0.03 * flip;
       }
-      this.x -= Math.sin(this.angle) * this.speed;
-      this.y -= Math.cos(this.angle) * this.speed;
+      if (this.controls.right) {
+        this.angle -= 0.03 * flip;
+      }
+    }
+    this.x -= Math.sin(this.angle) * this.speed;
+    this.y -= Math.cos(this.angle) * this.speed;
   }
 
   draw(ctx) {
@@ -67,5 +68,7 @@ class Car {
     ctx.fill();
 
     ctx.restore();
+
+    this.sensor.draw(ctx);
   }
 }
--- Virtual city/car/script.js
+++ Virtual city/car/script.js
@@ -1,14 +1,21 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
-canvas.width = 600;
-const car = new Car(100, 100, 30, 50);
+canvas.width = 200;
+const road = new Road(canvas.width / 2, canvas.width * 0.9);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50);
 
 animate();
 
 function animate() {
   car.update();
-  canvas.height = 600
+  canvas.height = window.innerHeight;
+
+  ctx.save();
+  ctx.translate(0, -car.y+canvas.height*0.7);
+  road.draw(ctx);
   car.draw(ctx);
+
+  ctx.restore();
   requestAnimationFrame(animate);
 }
