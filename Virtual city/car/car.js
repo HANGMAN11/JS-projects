083f12b9d792122528379dec6b60fe1d9a5b5813
Oleh Rytov <oleja.229@gmail.com>
2024-02-20 07:43:03 +0200

    added different neural nerwork features, and started implementing them

--- Virtual city/car/car.js
+++ Virtual city/car/car.js
@@ -1,5 +1,5 @@
 class Car {
-  constructor(x, y, width, height) {
+  constructor(x, y, width, height, controlType, maxSpeed = 7) {
     this.x = x;
     this.y = y;
     this.width = width;
@@ -7,32 +7,55 @@ class Car {
 
     this.speed = 0;
     this.acceleration = 0.2;
-    this.maxSpeed = 7;
+    this.maxSpeed = maxSpeed;
     this.friction = 0.05;
     this.angle = 0;
     this.damaged = false;
 
-    this.sensor = new Sensor(this);
-    this.controls = new Controls();
-  }
+    this.useBrain = controlType == "AI";
 
-  update(roadBorders) {
-    if(!this.damaged){
-    this.#move();
-    this.polygon = this.#createPolygon();
-    this.damaged = this.#assessDamage(roadBorders)
+    if (controlType != "DUMMY") {
+      this.sensor = new Sensor(this);
+      this.brain = new NeuralNetwork([this.sensor.rayCount, 6, 4]);
     }
-    this.sensor.update(roadBorders);
+    this.controls = new Controls(controlType);
   }
 
-  #assessDamage(roadBorders){
-    for(let i=0; i<roadBorders.length; i++){
-      if(polysIntersect(this.polygon, roadBorders[i])){
-        return true;
-        } 
+  update(roadBorders, traffic) {
+    if (!this.damaged) {
+      this.#move();
+      this.polygon = this.#createPolygon();
+      this.damaged = this.#assessDamage(roadBorders, traffic);
+    }
+    if (this.sensor) {
+      this.sensor.update(roadBorders, traffic);
+      const offsets = this.sensor.readings.map((s) =>
+        s == null ? 0 : 1 - s.offset
+      );
+      const outputs = NeuralNetwork.feedForward(offsets, this.brain);
+
+      if (this.useBrain) {
+        this.controls.forward = outputs[0];
+        this.controls.left = outputs[1];
+        this.controls.right = outputs[2];
+        this.controls.reverse = outputs[3];
       }
-      return false
     }
+  }
+
+  #assessDamage(roadBorders, traffic) {
+    for (let i = 0; i < roadBorders.length; i++) {
+      if (polysIntersect(this.polygon, roadBorders[i])) {
+        return true;
+      }
+    }
+    for (let i = 0; i < traffic.length; i++) {
+      if (polysIntersect(this.polygon, traffic[i].polygon)) {
+        return true;
+      }
+    }
+    return false;
+  }
 
   #createPolygon() {
     const points = [];
@@ -68,7 +91,6 @@ class Car {
     if (this.speed > this.maxSpeed) {
       this.speed = this.maxSpeed;
     }
-
     if (this.speed < -this.maxSpeed / 2) {
       this.speed = -this.maxSpeed / 2;
     }
@@ -82,9 +104,9 @@ class Car {
     if (Math.abs(this.speed) < this.friction) {
       this.speed = 0;
     }
+
     if (this.speed != 0) {
       const flip = this.speed > 0 ? 1 : -1;
-
       if (this.controls.left) {
         this.angle += 0.03 * flip;
       }
@@ -92,22 +114,26 @@ class Car {
         this.angle -= 0.03 * flip;
       }
     }
+
     this.x -= Math.sin(this.angle) * this.speed;
     this.y -= Math.cos(this.angle) * this.speed;
   }
 
-  draw(ctx) {
-    if(this.damaged){
-      ctx.fillStyle="gray";
-    } else{
-      ctx.fillStyle="black"
+  draw(ctx, color) {
+    if (this.damaged) {
+      ctx.fillStyle = "rgb(108, 107, 59)";
+    } else {
+      ctx.fillStyle = color;
     }
     ctx.beginPath();
     ctx.moveTo(this.polygon[0].x, this.polygon[0].y);
-    for(let i=1;i<this.polygon.length;i++){
-      ctx.lineTo(this.polygon[i].x, this.polygon[i].y)
+    for (let i = 1; i < this.polygon.length; i++) {
+      ctx.lineTo(this.polygon[i].x, this.polygon[i].y);
     }
     ctx.fill();
-    this.sensor.draw(ctx);
+
+    if (this.sensor) {
+      this.sensor.draw(ctx);
+    }
   }
 }
